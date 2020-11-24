95cac83231c603b16185c9f50ad72e7cc21ea5b3
sarahlcox <sarahl.cox.22@gmail.com>
2020-11-24 14:00:38 -0600

    testing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+// Testing to see push to main
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
