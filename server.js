30e97d021a29cabf86207b5e0ff7a74097b6f899
jcuush <jrcushing@sbcglobal.net>
2020-11-24 13:57:51 -0600

    test 2

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// test #2
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
