9707abe084c69527b2b09cbc732b61303dc2a632
Agnes Chueng <agneschueng@gmail.com>
2020-12-01 11:51:46 -0600

    add API method getHotels

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,6 +8,9 @@ export default {
     getFlight: function(flightInput){
       console.log("my FI", flightInput)
       return axios.post("/api/flights", flightInput)
+    },
+    getHotels: function(hotelsInput) {
+      return axios.post("/api/hotels", hotelsInput)
     }
 //   getBooks: function() {
 //     return axios.get("/api/books");
