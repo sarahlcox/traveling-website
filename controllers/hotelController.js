aeb0fd7ccdb1ae3cac4c8664862c5f343b459651
Agnes Chueng <agneschueng@gmail.com>
2020-12-01 11:10:47 -0600

    return res.json of data from getHotelList

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -3,10 +3,8 @@ const getHotelList = require("./hotelsHelper/hotelsList");
 
 const hotels = {
     getHotels: async function (req, res) {
-        await getLocationID(req.body.city2);
-        // const hotelsList = await getHotelList(req.body.city2);
-        // res.json(hotelsList);
-        // console.log(hotelsList);
+        const hotelsList = await getHotelList(req.body.city2);
+        res.json(hotelsList);
     }
 }
 
