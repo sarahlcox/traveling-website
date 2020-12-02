395d123eea8dd7dc2dd3139b20e63805b25dfba6
Agnes Chueng <agneschueng@gmail.com>
2020-12-02 11:39:22 -0600

    add table to pass in data from props

--- client/src/components/Hotels/hotels.js
+++ client/src/components/Hotels/hotels.js
@@ -1,28 +1,45 @@
 import React, { Component } from "react";
 import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
+import "./Hotels.css";
 // import "./style.css";
-class Hotels extends Component {
-    // Setting the component's initial state
-    state = {
-
-    };
-    render() {
-        return (
-                <Card className="single-card">
-                    {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
-                    <Card.Body>
-                    <Card.Title>Top Places to Stay</Card.Title>
-                    <Card.Text>
-                        This card has supporting text below as a natural lead-in to additional
-                        content.{' '}
-                    </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-        )
-    }
+function Hotels(props) {
+    return (
+            <Card className="single-card">
+                {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+                <Card.Body>
+                <Card.Title>Top Places to Stay</Card.Title>
+                <Card.Text>
+                    {(props.hotelInfo.hotelsData === []) ? "Please enter in a destination city" :
+                    (<Table striped bordered responsive>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Hotel</th>
+                                <th>Rate</th>
+                                <th>Stars</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {props.hotelInfo.hotelsData.map(hotel => {
+                                return (
+                                    <tr key={props.hotelInfo.hotelsData.indexOf(hotel)} >
+                                        <td><img src={hotel.image} alt={hotel.name} /></td>
+                                        <td>{hotel.name}</td>
+                                        <td>{hotel.price}</td>
+                                        <td>{hotel.star}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </Table>)}
+                </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+            </Card>
+    )
 }
 
 export default Hotels;
