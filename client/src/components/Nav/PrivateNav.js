9a8010d15c5fba30116d3336abfa3f28e4e8040e
sarahlcox <sarahl.cox.22@gmail.com>
2020-12-05 17:56:39 -0600

    updating front end files

---
+++ client/src/components/Nav/PrivateNav.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navbar,Nav} from 'react-bootstrap';
+import './Navbar.css';
+import whitelogoplaceholder from '../../Assets/whitelogoplaceholder.png';
+
+function PrivateNav() {
+    return (
+        <Navbar className="nav-style" expand="lg" sticky="top">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src= {whitelogoplaceholder}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+        <Nav.Link href="/privatehome">Home</Nav.Link>
+        <Nav.Link href="/signup">Account</Nav.Link>
+        </Nav>
+      </Navbar>
+    );
+  }
+  
+  export default PrivateNav;
+  
