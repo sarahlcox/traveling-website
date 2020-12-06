f3d6ba726ba4ef96f66e7a48e8d2673f7db4f209
Agnes Chueng <agneschueng@gmail.com>
2020-12-06 17:14:24 -0600

    set up get search function

--- controllers/saveController.js
+++ controllers/saveController.js
@@ -2,7 +2,7 @@ const { User, Search } = require("../models");
 
 const save = {
     saveSearch: (req, res) => {
-        console.log(req.body);
+        // console.log(req.body);
         Search.create(req.body)
         .then(({ _id }) => User.findOneAndUpdate({}, { $push: { search: _id } }, { new: true }))
         .then(userSearch => {
@@ -11,6 +11,22 @@ const save = {
         .catch(err => {
         res.json(err);
         });
+    },
+    getSearch: (req, res) => {
+        // console.log("backend id", req.params.id);
+        User.findById(req.params.id).then((data) => {
+            // console.log(data.search);
+            // let searches = 
+            data.search.map(entryId => {
+                Search.findById(entryId).then(searchRes => {
+                    console.log(searchRes);
+                    return searchRes;
+                    // res.json(searchRes); // returns one object only
+                })
+            })
+            // res.json(searches); // [null, null]
+            // console.log(searches);
+        })
     }
 };
 
--- routes/api/save.js
+++ routes/api/save.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 const saveController = require("../../controllers/saveController");
 
 // save search route
-router.route("/").post(saveController.saveSearch);
+router.route("/")
+    .post(saveController.saveSearch)
+    
+router.route("/:id")
+    .get(saveController.getSearch)
 
 module.exports = router;
