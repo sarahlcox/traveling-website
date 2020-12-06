c95b453f3d9075585da6406558013c59838e772e
Agnes Chueng <agneschueng@gmail.com>
2020-12-06 15:24:10 -0600

    add save post route

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -18,6 +18,6 @@ export default {
       return axios.post("/api/attractions", {city2:attractionsInput})
     },
     saveSearch: function(searchInput){
-      return axios.post("/api/savesearch", searchInput)
+      return axios.post("/api/save", searchInput)
     },
 };
