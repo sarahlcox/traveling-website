ae16427cd5481b0b5cd3b4f9c630dcf8efa5c812
Agnes Chueng <agneschueng@gmail.com>
2020-12-07 13:27:50 -0600

    clean up comments

--- controllers/saveController.js
+++ controllers/saveController.js
@@ -1,29 +1,15 @@
 const { User, Search } = require("../models");
 const mongojs = require("mongojs");
 
-async function getList(index) {
-    return index.map((index) => {
-
-        Search.findById(index).then(searchRes => {
-            console.log("get search res", searchRes);
-            return searchRes;
-            // res.json(searchRes); // returns one object only
-        })
-    })
-}
-
-
 const save = {
     saveSearch: (req, res) => {
         // console.log(req.body.userId);
-        const searchO = {     city1: req.body.city1,
+        const searchObj = {     city1: req.body.city1,
             city2: req.body.city2,
             stateCode: req.body.stateCode,
             outboundDate: req.body.outboundDate}
-        Search.create({
-         searchO
-        })
-            .then(({}) => User.findByIdAndUpdate({ _id: mongojs.ObjectId(req.body.userId) }, { $push: { search: searchO} }, { new: true }))
+        Search.create(searchObj)
+            .then(() => User.findByIdAndUpdate({ _id: mongojs.ObjectId(req.body.userId) }, { $push: { search: searchObj} }, { new: true }))
             .then(userSearch => {
                 res.json(userSearch);
             })
@@ -32,42 +18,11 @@ const save = {
             });
     },
     getSearch: (req, res) => {
-        // console.log("backend id", req.params.id);
         User.findById(req.params.id).then((data) => {
-            // console.log("data search",data.search);
             const mySearchList = data.search;
-            // console.log("mysearches",mySearchList)
-            res.json(mySearchList)
-
-        })
-       
+            res.json(mySearchList);
+        })       
     }
-
-    // returns [{},{}] array of empty object
-    /*
-    User.findById(req.params.id).then(async function (data) {
-        // console.log("data", data);
-        function searchList() {
-            let searchArr = [];
-            data.search.forEach(entryId => {
-                const searchObj = Search.findById(mongojs.ObjectId(entryId))
-                .then(searchRes => {
-                    // console.log("all searches", searchRes);
-                    return searchRes;
-                    // res.json(searchRes); // returns one object only
-                });
-                searchArr.push(searchObj);
-            });
-            return searchArr;
-        }
-        const savedSearches = await searchList();
-        res.json(savedSearches);
-        // res.json(searches); // [null, null]
-        // console.log(User.find().then(data => data));
-        res.json(data.search)
-    })
-    */
-
 };
 
 module.exports = save;
