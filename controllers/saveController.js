569904867915c2fac8f4857d70fec1bc001a317d
Agnes Chueng <agneschueng@gmail.com>
2020-12-07 13:29:10 -0600

    update comments

--- controllers/saveController.js
+++ controllers/saveController.js
@@ -2,12 +2,14 @@ const { User, Search } = require("../models");
 const mongojs = require("mongojs");
 
 const save = {
+    // post search info
     saveSearch: (req, res) => {
-        // console.log(req.body.userId);
-        const searchObj = {     city1: req.body.city1,
+        const searchObj = {
+            city1: req.body.city1,
             city2: req.body.city2,
             stateCode: req.body.stateCode,
-            outboundDate: req.body.outboundDate}
+            outboundDate: req.body.outboundDate
+        }
         Search.create(searchObj)
             .then(() => User.findByIdAndUpdate({ _id: mongojs.ObjectId(req.body.userId) }, { $push: { search: searchObj} }, { new: true }))
             .then(userSearch => {
@@ -17,6 +19,7 @@ const save = {
                 res.json(err);
             });
     },
+    // get search info stored in db - users
     getSearch: (req, res) => {
         User.findById(req.params.id).then((data) => {
             const mySearchList = data.search;
