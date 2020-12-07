f360157ec53c668ca1b752b383ec5a6060e8b12e
sarahlcox <sarahl.cox.22@gmail.com>
2020-12-07 14:28:51 -0600

    css changes

--- client/src/components/Layout/EmptyCard.js
+++ client/src/components/Layout/EmptyCard.js
@@ -5,7 +5,7 @@ function EmptyCard(props) {
     return (
         <Card className="single-card" >
             <Card.Body>
-                <Card.Title>{props.info} Information Not Available</Card.Title>
+                <Card.Title className="card-heading">{props.info} Information Not Available</Card.Title>
                 <Card.Text>
                 Please enter in your travel information to search for {props.info.toLowerCase()} details.
                 </Card.Text>
