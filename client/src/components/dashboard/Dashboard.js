612b0e5ddacb80f65543589d46c37c5aaf7921e7
Aaron Ray <aaray619@gmail.com>
2020-12-07 11:15:55 -0600

    mapping function works

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,7 +7,7 @@ import API from "../../utils/API";
 
 class Dashboard extends Component {
 
-state = []
+  state = []
 
 
   onLogoutClick = e => {
@@ -22,49 +22,61 @@ state = []
       this.setState(myResponse)
     })
   }
-  grabList = (state) => {
-    if(state[0]){
-    return(<h1>{state[0].city1}</h1>)
+  grabList = (mystate) => {
+    if (mystate[0]) {
+      const newState = Object.values(mystate);
+      console.log(newState);
+      let grabbedlist = newState.map(e => {
+        return (
+          <div>
+            <p>{e.city1} to {e.city2}</p>
+          </div>
+        )
+      })
+      return grabbedlist
     }
-    else{
-      return(<h1>no saved searches</h1>)
+    else {
+      return null
     }
+
   }
-render() {
+
+
+  render() {
     const { user } = this.props.auth;
     console.log("user id", this.props.auth.user.id);
     this.getSavedList(this.props.auth.user.id);
     console.log("state", this.state[0])
-return (
+    return (
       <div>
         <PrivateNav />
-          <div style={{ height: "75vh" }} className="container valign-wrapper">
-            <div className="row">
-              <div className="col s12 center-align">
-                <h4>
-                  <b>Hey there,</b> {user.name.split(" ")[0]}
-                  <p className="flow-text grey-text text-darken-1">
-                    You are logged into a{" "}
-                    <span style={{ fontFamily: "monospace" }}>Traveling Website</span> app 👏
+        <div style={{ height: "75vh" }} className="container valign-wrapper">
+          <div className="row">
+            <div className="col s12 center-align">
+              <h4>
+                <b>Hey there,</b> {user.name.split(" ")[0]}
+                <p className="flow-text grey-text text-darken-1">
+                  You are logged into a{" "}
+                  <span style={{ fontFamily: "monospace" }}>Traveling Website</span> app 👏
                   </p>
-                </h4>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  onClick={this.onLogoutClick}
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Logout
+              </h4>
+              <button
+                style={{
+                  width: "150px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                  marginTop: "1rem"
+                }}
+                onClick={this.onLogoutClick}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Logout
                 </button>
-              </div>
-                <h1>{this.grabList(this.state)}</h1>
             </div>
+            <h1>{this.grabList(this.state)}</h1>
           </div>
-    </div>
+        </div>
+      </div>
     );
   }
 }
