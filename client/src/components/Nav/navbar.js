a8f9274f461f60300bd72eaf4ce4f0176d0a30e7
sarahlcox <sarahl.cox.22@gmail.com>
2020-12-07 13:11:48 -0600

    updated styling and navbars

--- client/src/components/Nav/navbar.js
+++ client/src/components/Nav/navbar.js
@@ -2,37 +2,42 @@ import React from "react";
 import { Navbar,Nav,NavbarBrand} from 'react-bootstrap';
 import './Navbar.css';
 import TPWLogo from '../../Assets/TPWLogo.png';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
 
-
+// function navBrandRenderer() {
+//   var w= window.innerWidth;
+//   if (w>994px) {
+// }
 function MyNav() {
     return (
       <Navbar
       className="nav-style main nav-margins d-flex justify-content-between"
       expand="lg"
     >
-      <a className="link-style" to="/" style={{ flex: "1" }}>
-        <NavbarBrand className="logo-size" alt="logo">
+      <a className="link-style mobile-logo" to="/" style={{ flex: "1" }}>
+      <NavbarBrand className="logo-size" alt="logo">
+          <img
+            height="59.22"
+            width="259"
+            src={TPWLogo}
+          />
         </NavbarBrand>
       </a>
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="ml-auto" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mx-auto parent">
-        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/" className="resp-link">Home</Nav.Link>
           <Nav.Link>
           <Navbar.Brand href="#home">
           <img
             alt=""
             src= {TPWLogo}
-            className="d-inline-block align-top mx-auto"
+            className="d-inline-block align-top mx-auto resp-logo"
             id= "image-size"
           
           />{' '}
         </Navbar.Brand>
           </Nav.Link>
-          <Nav.Link href="/signup">Account</Nav.Link>
+          <Nav.Link href="/signup" className="resp-link">Account</Nav.Link>
         </Nav>
       </Navbar.Collapse>
       <div className="navbar-collapse collapse" style={{ flex: "1" }} />
