2c4f2254b95bb05afb959e351bc556f79093ed31
Agnes Chueng <agneschueng@gmail.com>
2020-12-07 01:03:49 -0600

    pass in user id in saveSearch

--- client/src/pages/PrivateHome.js
+++ client/src/pages/PrivateHome.js
@@ -38,6 +38,7 @@ function PrivateHome(props) {
 
     function saveInput(){
         API.saveSearch({ 
+            userId: props.userId.id,
             city1: formObject.city1,
             city2: formObject.city2,
             stateCode: formObject.stateCode,
