4d0111d9eea8876a2be52c6f3e22b878d156f32b
Agnes Chueng <agneschueng@gmail.com>
2020-12-09 17:21:12 -0600

    change class component to function component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,75 +4,80 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import PrivateNav from "../Nav/PrivateNav.js";
-import SearchesList from "./searchedList"
+import Greeting from "./Greeting";
+import SearchInfo from "../SavedSearch/SearchInfo";
+import SavedList from "../SavedSearch/SavedList";
 import API from "../../utils/API";
 import "./Dashboard.css";
 
-class Dashboard extends Component {
+function Dashboard(props) {
   // define state
-  state = []
+  const [savedState, setSavedState] = React.useState([]);
+  const [searchObj, setSearchObj] = React.useState({});
 
-  onLogoutClick = e => {
+  const onLogoutClick = e => {
     e.preventDefault();
-    this.props.logoutUser();
+    props.logoutUser();
   };
-  // when component moounts, get saved searches list from database
-  componentDidMount() {
-    this.getSavedList(this.props.auth.user.id);
-  }
+
+  const { user } = props.auth;
+  // get saved searches list from database
+  React.useEffect(() => getSavedList(props.auth.user.id));
+
   // define function to get data from database
-  getSavedList = userId => {
+  const getSavedList = userId => {
     API.getSearch(userId).then(response => {
-      // console.log(response.data);
       let myResponse = response.data
-      this.setState(myResponse)
+      setSavedState(myResponse)
     })
   }
 
-  render() {
-    const { user } = this.props.auth;
-    return (
-      <div>
-        <PrivateNav />
-        <Container fluid className="cont mt-4">
-          <Row>
+  const handleClick = event => {
+    const index = event.target.parentElement.getAttribute("data-id");
+    const entryObj = {
+      city1: savedState[index].city1,
+      city2: savedState[index].city2,
+      stateCode: savedState[index].stateCode,
+      outboundDate: savedState[index].outboundDate
+    }
+    setSearchObj(entryObj);
+}
+
+  return (
+    <div>
+      <PrivateNav />
+      <Container fluid className="cont mt-4">
+        <Row>
             <Col 
             xs={{span: 12, order: 2}}
             md={{span: 5, order: 1}} 
             xl={{span: 4, order: 1}}>
-              <SearchesList list={(this.state[0]) ? (Object.values(this.state)) : (null)} />
+              <SavedList 
+              list={(savedState[0]) ? (Object.values(savedState)) : (null)} 
+              handleClick={handleClick}
+              />
             </Col>
             <Col 
             xs={{span: 12, order: 1}} 
             md={{span: 7, order: 2}} 
             xl={{span: 8, order: 2}}>
-              <Jumbotron className="greeting mx-auto">
-                <h1 className="mb-4"><b>Hey there,</b> {user.name.split(" ")[0]}</h1>
-                <h3 className="mb-2">
-                  You are logged into {" "}
-                  <span>Travel Pocket Wizard</span>
-                </h3>
-                <Button
-                  onClick={this.onLogoutClick}
-                  size="lg"
-                  className="logout-btn my-2"
-                >
-                  Logout
-                </Button>
-              </Jumbotron>
+              <Greeting
+                username={user.name.split(" ")[0]}
+                logout={onLogoutClick}
+              />
             </Col>
             <Col 
             xs={{span: 12, order: 12}} 
             md={{span: 7, offset: 5, order:12}} 
             xl={{span: 8, offset: 4, order:12}}>
-              <h1>Saved Search Travel Details Go Here</h1>
+              <SearchInfo search={(!searchObj.city1) ? null : searchObj}/>
             </Col>
           </Row>
         </Container>
       </div>
-    );
-  }
+  );
 }
+
 Dashboard.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
