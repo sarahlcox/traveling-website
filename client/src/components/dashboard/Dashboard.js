a4b35b70c1639270af1e20b7bdc35be86c958249
Agnes Chueng <agneschueng@gmail.com>
2020-12-09 14:06:08 -0600

    optimize condition to pass in data to searchList

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -28,34 +28,24 @@ class Dashboard extends Component {
       this.setState(myResponse)
     })
   }
-  // get list of saved searches from state
-  grabList = (mystate) => {
-    if (mystate[0]) {
-      const newState = Object.values(mystate);
-      // console.log(newState);
-        return (
-        <SearchesList list = {newState}/>
-        )
-    }
-    else {
-      return <p>no info</p>
-    }
-  }
 
   render() {
     const { user } = this.props.auth;
-    // console.log("user id", this.props.auth.user.id);
-    // this.getSavedList(this.props.auth.user.id);
-    // console.log("state", this.state[0])
     return (
       <div>
         <PrivateNav />
         <Container fluid className="cont mt-4">
           <Row>
-            <Col xs={{span: 12, order: 2}} md={{span: 5, order: 1}} xl={{span: 4, order: 1}}>
-              {this.grabList(this.state)}
+            <Col 
+            xs={{span: 12, order: 2}}
+            md={{span: 5, order: 1}} 
+            xl={{span: 4, order: 1}}>
+              <SearchesList list={(this.state[0]) ? (Object.values(this.state)) : (null)} />
             </Col>
-            <Col xs={{span: 12, order: 1}} md={{span: 7, order: 2}} xl={{span: 8, order: 2}}>
+            <Col 
+            xs={{span: 12, order: 1}} 
+            md={{span: 7, order: 2}} 
+            xl={{span: 8, order: 2}}>
               <Jumbotron className="greeting mx-auto">
                 <h1 className="mb-4"><b>Hey there,</b> {user.name.split(" ")[0]}</h1>
                 <h3 className="mb-2">
@@ -71,8 +61,11 @@ class Dashboard extends Component {
                 </Button>
               </Jumbotron>
             </Col>
-            <Col xs={{span: 12, order: 12}}  md={{span: 7, offset: 5, order:12}} xl={{span: 8, offset: 4, order:12}}>
-              <h1>Hello World</h1>
+            <Col 
+            xs={{span: 12, order: 12}} 
+            md={{span: 7, offset: 5, order:12}} 
+            xl={{span: 8, offset: 4, order:12}}>
+              <h1>Saved Search Travel Details Go Here</h1>
             </Col>
           </Row>
         </Container>
